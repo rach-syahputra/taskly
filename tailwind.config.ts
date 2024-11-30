a94dcc6bf74a520153c7c878c5a3d63dd7e26e54
rach-syahputra <rach.syahputra@gmail.com>
2024-11-30 08:26:20 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: '#242123',
+        primary: '#DE5283',
+        secondary: '#B94870'
       },
-    },
+      fontFamily: {
+        geistSans: ['var(--font-geist-sans)'],
+        geistMono: ['var(--font-geist-mono)']
+      }
+    }
   },
-  plugins: [],
-} satisfies Config;
+  plugins: []
+} satisfies Config
